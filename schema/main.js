dd492da2ceece9b95a00477ea693693593fe6707
Luisk Hernandez <luis.macias@koombea.com>
2016-10-06 15:01:55 -0500

    Add diceRoll to the schema

--- schema/main.js
+++ schema/main.js
@@ -12,9 +12,27 @@ const queryType =  new GraphQLObjectType({
     hello: {
       type: GraphQLString,
       resolve: () => 'world'
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      args: {
+        count: {
+          type: GraphQLInt,
+          defaultValue: 2
+        }
+      },
+      resolve: (_, args) =>{
+        let rolls = []
+        for (let i = 0; i < args.count; i++) {
+          rolls.push(roll());
+        }
+        return rolls;
+      }
     }
   }
 });
+
+const roll = () => Math.floor(6 * Math.random()) + 1;
 const mySchema = new GraphQLSchema({
   query: queryType
 });
